8836288fb23381f084158c36a74a00402717603d
Julio-SC <jhver2001@gmail.com>
2024-09-17 12:14:31 -0700

    Exercise_3_Solved

---
+++ js/exercise3.js
@@ -0,0 +1,11 @@
+let number1 = 22
+let number2 = 24
+let number3 = 19
+let largest = number1
+
+if (number2 > largest){
+    largest = number2
+} else if (number3 > largest){
+    largest = number3
+}
+console.log(largest)
