6e1dca16b5de7b1f46af9d601d95beabe23e99a0
Julio-SC <jhver2001@gmail.com>
2024-09-17 18:54:27 -0700

    Exercise_5_Solved

---
+++ js/exercise5.js
@@ -0,0 +1,12 @@
+let student = "Mike"
+let mark = 85
+
+if (mark >= 80){
+    console.log("A")
+}else if (mark >= 60){
+    console.log("B")
+}else if (mark >= 40){
+    console.log("C")
+}else if (mark < 40){
+    console.log("No Grade")
+}
